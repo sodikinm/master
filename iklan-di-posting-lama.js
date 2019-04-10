3bd527083547a17fa10ebc2ac1afecd8f1afd24c
sodikinm <45006468+sodikinm@users.noreply.github.com>
2019-04-10 16:57:50 +0700

    Create iklan-di-posting-lama.js

---
+++ iklan-di-posting-lama.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+//<![CDATA[
+(function() { var ad = document.createElement('script'); ad.type = 'text/javascript'; ad.async = true; ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ad, sc); })();
+//]]>
+</script>
